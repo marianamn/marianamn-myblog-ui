7cd49f53d72885b974a011d89db90ca30197c94a
Mariyana Naidenova <mariananaidenova@mail.bg>
2019-02-23 12:23:58 +0200

    api end point fix

--- public/src/configs/index.tsx
+++ public/src/configs/index.tsx
@@ -1,6 +1,6 @@
 const CoreConfig = {
-  //apiEndpoint: "https://marianamn-myblog-api.herokuapp.com/api",
-  apiEndpoint: "http://localhost:3001/api",
+  apiEndpoint: "https://marianamn-myblog-api.herokuapp.com/api",
+  //apiEndpoint: "http://localhost:3001/api",
 };
 
 export default CoreConfig.apiEndpoint;
